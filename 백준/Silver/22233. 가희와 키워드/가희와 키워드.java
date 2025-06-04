097781361974623840705572a4868d4c5a5f3fe1
kimjaegyeong <50646904+kimjaegyeong@users.noreply.github.com>
2025-06-04 19:03:54 +0900

    [Silver III] Title: 가희와 키워드, Time: 2960 ms, Memory: 272916 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+    static int N;
+    static int M;
+    static int cnt =0;
+    static HashSet<String> set = new HashSet<String>();
+    
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<N; i++){
+		    set.add(br.readLine());
+		}
+		
+		
+		for(int i=0; i<M; i++){
+		    st = new StringTokenizer(br.readLine(),",");
+		    int it = st.countTokens(); 
+            for(int j=0; j<it; j++){
+                String word = st.nextToken();
+                
+                if(set.contains(word)){
+                    set.remove(word);
+                }
+            }
+		    
+		    System.out.println(set.size());
+		}
+	}
+}
