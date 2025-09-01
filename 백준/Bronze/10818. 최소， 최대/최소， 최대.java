49cc4684aa0608bfe58b00caeb52e3a8436a07b8
kimjaegyeong <50646904+kimjaegyeong@users.noreply.github.com>
2025-09-02 02:24:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 384 ms, Memory: 120272 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,6 +4,9 @@ public class Main
 {
     static int N;
     static int[] map;
+    
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -11,10 +14,11 @@ public class Main
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++){
 		    map[i] = Integer.parseInt(st.nextToken());
+		    min = Math.min(min, map[i]);
+		    max = Math.max(max, map[i]);
+		    
 		}
 		
-		Arrays.sort(map);
-		
-		System.out.println(map[0] + " " + map[N-1]);
+		System.out.println(min + " " + max);
 	}
 }
